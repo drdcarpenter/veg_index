ed67c419ac25aef015b706b144a5bbd64f1f224f
Dan Carpenter <drdcarpenter@googlemail.com>
2020-07-22 19:26:13 +0100

    update code

--- processing_uav_imagery.R
+++ processing_uav_imagery.R
@@ -1,21 +1,23 @@
 library(raster)
 
-sonning <- raster("C:/Users/TVERC/Documents/Battlemead UAV/Sonning/sonning.tif")
+sonning <- raster("sonning.tif")
 plot(sonning)
 
 x <- log(sonning)
 plot(x)
 
-require(uavRst)
-plotRGB(sonning, 1,2,3)
-
-sonningr <- raster("C:/Users/TVERC/Documents/Battlemead UAV/Sonning/sonning.tif", band = 1)
-sonningg <- raster("C:/Users/TVERC/Documents/Battlemead UAV/Sonning/sonning.tif", band = 2)
-sonningb <- raster("C:/Users/TVERC/Documents/Battlemead UAV/Sonning/sonning.tif", band = 3)
+# read in each band separately and then combine into rgb stack
+sonningr <- raster("sonning.tif", band = 1)
+sonningg <- raster("sonning.tif", band = 2)
+sonningb <- raster("sonning.tif", band = 3)
 
 sonningrgb <- stack(sonningr, sonningg, sonningb)
 plotRGB(sonningrgb, 1,2,3)
 
+# TODO normalise image?
+# where was the reference to this?
+
+# process through indices
 sexg <- exg(sonningrgb, 1, 2, 3)
 plot(sexg)
 
